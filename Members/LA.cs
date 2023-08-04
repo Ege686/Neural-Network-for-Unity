9805d4ea9875d8ab0a560b0316ff3f111e510a05
Ege686 <64255975+Ege686@users.noreply.github.com>
2023-08-04 18:10:38 +0300

    Update LA.cs

--- Members/LA.cs
+++ Members/LA.cs
@@ -5,11 +5,17 @@ public class LA
     int layer_count = 0;
     Functions.ActivationDelegate activation;
     Functions.LossDelegate loss;
-    public LA(int layer_count, params Functions.ActivationDelegate[] activation)
+    float dropout_p = 0;
+    public LA(int layer_count, Functions.ActivationDelegate activation)
     {
         this.layer_count = layer_count;
-        if (activation.Length != 0)
-            this.activation = activation[0];
+        this.activation = activation;
+    }
+    public LA(int layer_count, Functions.ActivationDelegate activation, float dropout_p)
+    {
+        this.layer_count = layer_count;
+        this.activation = activation;
+        this.dropout_p = dropout_p;
     }
     public LA(int layer_count, Functions.LossDelegate loss)
     {
@@ -17,6 +23,7 @@ public class LA
         this.loss = loss;
     }
     public int LayerCount { get { return layer_count; } }
+    public float DropoutP { get { return dropout_p; } }
     public Functions.ActivationDelegate Activation { get { return activation; } }
     public Functions.LossDelegate Loss { get { return loss; } }
 }
