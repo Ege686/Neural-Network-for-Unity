c4c9113ddb2faca5c0260a2547ff3421aab6ffe5
Ege686 <64255975+Ege686@users.noreply.github.com>
2023-09-15 09:51:41 +0300

    Update Functions.cs

--- Members/Functions.cs
+++ Members/Functions.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System.Collections;
 public class Functions
 {
     public delegate float ActivationDelegate(float x, bool derivative, LossDelegate loss, params float[] a);
@@ -7,8 +7,9 @@ public class Functions
 
     public float Identity(float x, bool derivative, LossDelegate loss, params float[] a)
     {
-        if (!derivative) return x;
-        else return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) : 1 * 1;
+        float b = 1f;
+        if (!derivative) return x*b;
+        else return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true)*b : b;
     }
     public float BinaryStep(float x, bool derivative, LossDelegate loss, params float[] a)
     {
@@ -19,7 +20,7 @@ public class Functions
     {
         if (!derivative) { if (x < 0) return 0; else return x; }
 
-        else { if (x > 0) return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) : 1 * 1; else return 0; }
+        else { if (x > 0) return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) : 1; else return 0; }
     }
     public float LeakyReLU(float x, bool derivative, LossDelegate loss, params float[] a)
     {
@@ -38,24 +39,24 @@ public class Functions
         if (a.Length == 0) { Debug.LogError("ELU function needs a extra parameter. Please enter a value!"); return 0; }
         float A = a[0];
         if (!derivative) { if (x >= 0) return x; else return A * (Mathf.Exp(x) - 1); }
-        else { float X =Mathf.Log(x/A); if (x >= 0) return 1; else return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) : 1 * A * Mathf.Exp(X); }
+        else { float X =Mathf.Log(x/A); if (x >= 0) return 1; else return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) * 1 * A * Mathf.Exp(X) : 1 * A * Mathf.Exp(X); }
     }
     public float Sigmoid(float x, bool derivative, LossDelegate loss, params float[] a)
     {
         float X = 1 / (1 + Mathf.Exp(-x));
         if (!derivative) return X;
-        else return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) : 1 * x * (1 - x);
+        else return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true)* x * (1 - x) : x * (1 - x);
     }
     public float Softplus(float x, bool derivative, LossDelegate loss, params float[] a)
     {
         if (!derivative) return Mathf.Log(1 + Mathf.Exp(x));
-        else { float X =Mathf.Log(Mathf.Exp(x)-1); return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) : 1 * 1 / (1 + Mathf.Exp(-X)); }
+        else { float X =Mathf.Log(Mathf.Exp(x)-1); return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) * (1 / (1 + Mathf.Exp(-X))) :  1 / (1 + Mathf.Exp(-X)); }
     }
     public float TanH(float x, bool derivative, LossDelegate loss, params float[] a)
     {
         float X = (2 / (1 + Mathf.Exp(-2 * x))) - 1;
         if (!derivative) return X;
-        else return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) : 1 * 1 - (x * x);
+        else return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) * (1 - (x * x)) : (1 - (x * x));
     }
     public float SiLU(float x, bool derivative, LossDelegate loss, params float[] a)
     {
@@ -65,7 +66,7 @@ public class Functions
     public float Gaussian(float x, bool derivative, LossDelegate loss, params float[] a)
     {
         if (!derivative) return Mathf.Exp(-x * x);
-        else { float X =Mathf.Sqrt(-Mathf.Log(x)); return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true) : 1 * -2 * X * Mathf.Exp(-X * X); }
+        else { float X =Mathf.Sqrt(-Mathf.Log(x)); return (loss != null) ? loss(a[a.Length - 1], x, (a.Length - 1) / 2, true)* -2 * X * Mathf.Exp(-X * X) : -2 * X * Mathf.Exp(-X * X); }
     }
     public float Softmax(float x, bool derivative, LossDelegate loss,params float[] a)
     {
@@ -95,7 +96,7 @@ public class Functions
     //Mean Squared Error
     public float MSE(float t, float p, int node_count, bool derivative)
     {
-        return (2 / node_count) * (t - p);
+        return 2f /node_count* (p - t);
     }
     //Categorical Cross Entropy
     public float CCE(float t, float p, int node_count, bool derivative)
@@ -113,9 +114,10 @@ public class Functions
     //Binary Cross Entropy
     public float BCE(float t, float p, int node_count, bool derivative)
     {
-        float epsilon = 1e-6f;
-        p = Mathf.Clamp(p, epsilon, 1 - epsilon);
+        float epsilon = 1e-3f;
+        if (p == 0||p==1) p += epsilon;
         float x = -((t / p) - ((1 - t) / (1 - p)));
+        //Debug.Log(t + " " + p + " " + x);
         return x;
     }
     //Mean Absolute Percentage Error
